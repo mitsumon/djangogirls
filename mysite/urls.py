8ca6b34a781362b5fb73eacb326333171307cacf
mitsuhito.mondo <mitsuhito.mondo@gmail.com>
2021-10-22 17:21:46 +0900

    Added requied username and password to login

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth import views
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/login/', views.LoginView.as_view(), name='login'),
+    path('accounts/logout/', views.LogoutView.as_view(next_page='/'), name='logout'),
     path('', include('blog.urls')),
 ]
